f79f57bfd12d0bdd9748807ccdad1848d2e22564
sadayuki-matsuno <sadayuki.matsuno@gmail.com>
2020-03-04 21:28:18 +0900

    fix

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const jobStatusSuccess = "Success"
-const jobStatusFailure = "Failure"
-const jobStatusPending = "Pending"
-
-const success = 'success'
-const failure = 'failure'
-const pending = 'pending'
 
 async function run() {
   try {
+    const jobStatusSuccess = 'Success'
+    const jobStatusFailure = 'Failure'
+    const jobStatusPending = 'Pending'
+    const success = 'success'
+    const failure = 'failure'
+    const pending = 'pending'
+
     const context = github.context;
     const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
 
-    const token = core.getInput("token", {required: true});
-    const jobStatus = core.getInput("status", {required: true});
-    const url = core.getInput("log-url", {required: false}) || defaultUrl;
-    const description = core.getInput("description", {required: false});
-    const env = core.getInput("environment", {required: false});
-    const envUrl = core.getInput("environment-url", {required: false});
+    const token = core.getInput('token', {required: true});
+    const jobStatus = core.getInput('status', {required: true});
+    const url = core.getInput('log-url', {required: false}) || defaultUrl;
+    const description = core.getInput('description', {required: false});
+    const env = core.getInput('environment', {required: false});
+    const envUrl = core.getInput('environment-url', {required: false});
 
     let status = pending
     if (jobStatus == jobStatusSuccess) {
